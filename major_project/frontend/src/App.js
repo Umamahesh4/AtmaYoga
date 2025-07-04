4b07612541484c589fc4c842d29f9a857f1a45ef
Shibi404 <shibisbs123@gmail.com>
2025-07-04 23:12:07 +0530

    AtmaYoga: Restructure project folders and clean up files
    - Organized components into /components and pages into /pages

--- major_project/frontend/src/App.js
+++ major_project/frontend/src/App.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
-import Home from './pages/Home/Home'
-import Yoga from './pages/Yoga/Yoga'
-import About from './pages/About/About'
-import Tutorials from './pages/Tutorials/Tutorials'
+import Home from './pages/home/home'
 
 import './App.css'
 
@@ -13,9 +10,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/start' element={<Yoga />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/tutorials' element={<Tutorials />} />
       </Routes>
     </Router>
   )
---
+++ major_project/frontend/src/pages/home/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to AtmaYoga</h1>
+    </div>
+  );
+}
